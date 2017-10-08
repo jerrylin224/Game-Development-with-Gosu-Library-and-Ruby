c8b7642ad21b9ff2a16169949bbff8a20888d398
Jiahung Lin <pk60905@hotmail.com>
2017-10-08 11:52:15 +0800

    Make apple disappear while be eaten by snake

--- snake/snake.rb
+++ snake/snake.rb
@@ -1,12 +1,17 @@
 require 'gosu'
 
 class Apple
+  attr_reader :pos_x, :pos_y
+
   def initialize(game)
     @game = game
-    x = (rand * 800).round
-    y = (rand * 600).round
-    c = Gosu::Color::GREEN
-    @game.draw_quad(x, y, c, x + 10, y, c, x + 10, y + 10, c, x, y + 10, c)
+    @pos_x = (rand * 800).round
+    @pos_y = (rand * 600).round
+    @c = Gosu::Color::RED
+  end
+
+  def draw
+    @game.draw_quad(@pos_x, @pos_y, @c, @pos_x + 10, @pos_y, @c, @pos_x + 10, @pos_y + 10, @c, @pos_x, @pos_y + 10, @c)
   end
 end
 
@@ -19,18 +24,26 @@ class Game < Gosu::Window
     @y = 200
     @x_direction = 3
     @y_direction = 0
-   #  @apple = Apple.new(self)
+    @apple = Apple.new(self)
+    @visible = 1
   end
 
   def draw
     c = Gosu::Color::GREEN
-    draw_quad(@x, @y, c, @x + 10, @y, c, @x + 10, @y + 10, c, @x, @y + 10, c)
+    draw_quad(@x, @y, c, @x + 10, @y, c, @x + 10, @y + 10, c, @x, @y + 10, c) if @visible > 0
+    @apple.draw
   end
 
   def update
     @position = [@x += @x_direction, @y += @y_direction]
+    collect_apple
   end
 
+  def collect_apple
+    if Gosu.distance(@x, @y, @apple.pos_x, @apple.pos_y) < 10
+      @apple = Apple.new(self)
+    end
+  end
 
   def button_down(id)
     if id == Gosu::KB_RIGHT && @x_direction != -3
@@ -52,8 +65,13 @@ class Game < Gosu::Window
       @x_direction = 0
       @y_direction = -3
     end
+
+    if id == Gosu::KB_ESCAPE
+      close
+    end
   end
 end
 
 game = Game.new
 game.show
+
